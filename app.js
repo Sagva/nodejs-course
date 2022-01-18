0187b36c3404f2adfe3fe25a75c51b2a13f49161
Elena <sagva2014@gmail.com>
2022-01-18 10:39:56 +0100

    redirecting requests

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require('http')
-
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     const url = req.url
+    const method = req.method
+
     if(url === '/') { //listen a request at localhost:3000
 
         res.setHeader('Content-Type', 'text/html')
@@ -11,6 +13,12 @@ const server = http.createServer((req, res) => {
         res.write('<body><h1>Hello from Nodejs</h1> <form action="/message" method="POST"><input type="text" name="message"></input><button type="submit">Submit</button></form></body>')
         res.write('</html>')
         return res.end()
+    }
+    if(url === '/message' && method === 'POST') { //listen a request at localhost:3000/message
+        fs.writeFileSync('message.txt', 'dummy text') //create a file with "dummy text" inside of it
+       res.statusCode = 302; 
+       res.setHeader('Location', '/') //redirection
+       return res.end()
     }
 })
 
