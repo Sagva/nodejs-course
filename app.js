48e94351eed55ad37d90190a5ab807b16532f1b0
Elena <sagva2014@gmail.com>
2022-01-18 16:09:37 +0100

    using Express.Router()

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
-const http = require('http')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const express = require('express')
+const app = express();
 
-const bodyParser = require('body-parser')
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-const app = express()
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/add-product', (req, res, next) => {
-    
-    res.send('<form action="/product" method="POST"><input type="text" name="title"> <button type="submit">Add a product</button></input></form>')
-})
-app.post('/product', (req, res, next) => {
-    console.log('req.body', req.body)
-    res.redirect('/')
-})
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express</h1>')
-})
-
-
-const server = http.createServer(app)
-
-server.listen(3000)
+app.listen(3000);
