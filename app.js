a7197b57423fac51355f8c621702e35319367a30
Elena <sagva2014@gmail.com>
2022-01-18 09:59:54 +0100

    Sending response

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const http = require('http')
 
 const server = http.createServer((req, res) => {
     // console.log('req', req)
-    console.log('req.method', req.method)
-    console.log('req.headers', req.headers)
-    console.log('req.url', req.url)
-    //  process.exit()
+    // console.log('req.method', req.method)
+    // console.log('req.headers', req.headers)
+    // console.log('req.url', req.url)
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head><title>My first page</title></head>')
+    res.write('<body><h1>Hello from Nodejs</h1></body>')
+    res.write('</html>')
+    res.end()
+
 })
 
 server.listen(3000)
