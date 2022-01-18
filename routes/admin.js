64f852c5fd3eda5835b89249eb6dd88e0dffd229
Elena Myadzeleva <sagva2014@gmail.com>
2022-01-18 22:30:28 +0100

    Changed path.join to helping function

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const path = require("path");
 const router = express.Router();
+const rootDir = require("../utils/path");
 
-router.use("/add-product", (req, res, next) => {
+// /admin/add-product => GET
+router.get("/add-product", (req, res, next) => {
   // res.send('<form action="/product" method="POST"><input type="text" name="title"> <button type="submit">Add a product</button></input></form>')
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
+
+// /admin/product => POST
 router.post("/product", (req, res, next) => {
   console.log("req.body", req.body);
   res.redirect("/");
--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const path = require("path"); //path is a core module
 const router = express.Router();
+const rootDir = require("../utils/path");
+console.log(`rootDir`, rootDir);
 
 router.get("/", (req, res, next) => {
   // res.send('<h1>Hello from Express</h1>')
   //instead of writing and sending html here we will send a html-file that we created before
   // res.sendFile('/views/shop.html') we want to build a path like that
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
-  //__dirname - global variable that holds the absolute path in the operating system to the folder where we use it
-  //dirname will point to routes-folder,
-  // ../ means go up one level, so it will point to entire project folder
-  
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  //rootDir gives the absolute path to the project folder (in the operating system)
 });
 
 module.exports = router;
