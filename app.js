17f152ccf5ad47788206e8e67d2ca1748dd77b6b
Elena <sagva2014@gmail.com>
2022-01-18 10:13:50 +0100

    routing requests

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ const http = require('http')
 
 
 const server = http.createServer((req, res) => {
-    // console.log('req', req)
-    // console.log('req.method', req.method)
-    // console.log('req.headers', req.headers)
-    // console.log('req.url', req.url)
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<html>')
-    res.write('<head><title>My first page</title></head>')
-    res.write('<body><h1>Hello from Nodejs</h1></body>')
-    res.write('</html>')
-    res.end()
+    const url = req.url
+    if(url === '/') { //listen a request at localhost:3000
 
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<html>')
+        res.write('<head><title>Enter Message</title></head>')
+        res.write('<body><h1>Hello from Nodejs</h1> <form action="/message" method="POST"><input type="text" name="message"></input><button type="submit">Submit</button></form></body>')
+        res.write('</html>')
+        return res.end()
+    }
 })
 
 server.listen(3000)
