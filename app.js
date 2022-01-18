fb35263ada405e0fef215bb596dec09a98d6e551
Elena Myadzeleva <sagva2014@gmail.com>
2022-01-18 21:33:07 +0100

    Added 404-page, page not found

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 
-const adminRoutes = require('./routes/admin'); 
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>')
-})
+  //   res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  //don't need to add '../' here because we in the app.js file and the __dirpath will point to whole project folder
+});
 app.listen(3000);
