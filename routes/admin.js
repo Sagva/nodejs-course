b415698d4c5f7f13a68dd219fc6a531baf471e69
Elena Myadzeleva <sagva2014@gmail.com>
2022-01-18 21:25:13 +0100

    serving html pages (with helf of path core module)

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,14 @@
-const express = require('express')
+const express = require("express");
+const path = require("path");
+const router = express.Router();
 
-const router = express.Router()
+router.use("/add-product", (req, res, next) => {
+  // res.send('<form action="/product" method="POST"><input type="text" name="title"> <button type="submit">Add a product</button></input></form>')
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+});
+router.post("/product", (req, res, next) => {
+  console.log("req.body", req.body);
+  res.redirect("/");
+});
 
-router.use('/add-product', (req, res, next) => {
-    
-    res.send('<form action="/product" method="POST"><input type="text" name="title"> <button type="submit">Add a product</button></input></form>')
-})
-router.post('/product', (req, res, next) => {
-    console.log('req.body', req.body)
-    res.redirect('/')
-})
-
-module.exports = router
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,16 @@
-const express = require('express')
+const express = require("express");
+const path = require("path"); //path is a core module
+const router = express.Router();
 
-const router = express.Router()
+router.get("/", (req, res, next) => {
+  // res.send('<h1>Hello from Express</h1>')
+  //instead of writing and sending html here we will send a html-file that we created before
+  // res.sendFile('/views/shop.html') we want to build a path like that
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  //__dirname - global variable that holds the absolute path in the operating system to the folder where we use it
+  //dirname will point to routes-folder,
+  // ../ means go up one level, so it will point to entire project folder
+  
+});
 
-router.get('/', (req, res, next) => {
-    res.send('<h1>Hello from Express</h1>')
-})
-
-module.exports = router 
+module.exports = router;
