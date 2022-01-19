725d9352248b729f117cff8c4b0609e346340915
Elena <sagva2014@gmail.com>
2022-01-19 11:21:37 +0100

    using Ejs templating engine

--- app.js
+++ app.js
@@ -2,32 +2,23 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars'); // importing handlebars for using it as a template engine
+
 const app = express();
 
-//Pug was build-in in node thats why we use it without importing and registering
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-  })
-);
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: null });
 });
 
 app.listen(3000);
