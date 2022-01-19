602507adc6139fe7a20096366c572ad24935ba0c
Elena <sagva2014@gmail.com>
2022-01-19 13:39:06 +0100

    Starting set up for section

--- models/product.js
+++ models/product.js
@@ -1,37 +1,42 @@
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path');
 
-const path = require('path')
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
-    }
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
 
-    save() {
+  }
 
-        //'data' folder name where to store created file, 'products.json' - file name, that we want to create
-        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json' )
-        fs.readFile(p, (err, fileContent) => {
-            let products = []
-            if(!err) {// if where is no error, that means that file is already created
-                console.log(`fileContent`, fileContent)
-                products = JSON.parse(fileContent);
-            }
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err)=> {
-                console.log(`err`, err)
-            })
-        })
-    }
+  save() {
+      this.id = Math.random().toString()
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    static fetchAll(cb) { //cb - calback function that will be executed when fetchAll is done. When we are calling fetchAll we will pass a function it is being aware of being call which holds the data we want to return
-        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json' )
-        fs.readFile(p, (err, fileContent) => {
-            if(err) {//if there is an error, that means that there is no products yet 
-               cb([])
-            }
-            cb(JSON.parse(fileContent))
-        })
-       
-    }
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
