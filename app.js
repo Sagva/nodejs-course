ee2e0f0a3709b311147e38f31c655a0f61516ed6
Elena <sagva2014@gmail.com>
2022-01-19 10:47:33 +0100

    installing and using Handlebars

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const expressHbs = require('express-handlebars'); // importing handlebars for using it as a template engine
 const app = express();
 
-app.set('view engine', 'pug');
+//Pug was build-in in node thats why we use it without importing and registering
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+  })
+);
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
