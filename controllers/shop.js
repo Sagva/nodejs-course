2a2b090e52d4892449513093c467a07e7b9fc4a4
Elena <sagva2014@gmail.com>
2022-01-19 14:55:11 +0100

    passing data with POST requests

--- controllers/shop.js
+++ controllers/shop.js
@@ -36,6 +36,11 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId
+  console.log(`req.body`, req.body)
+  res.redirect('/cart')
+};
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
@@ -50,3 +55,4 @@ exports.getCheckout = (req, res, next) => {
     pageTitle: 'Checkout'
   });
 };
+
