c70faad5f8ff3ea68c90e3b5c854ce2a4335952b
Elena <sagva2014@gmail.com>
2022-01-20 16:57:00 +0100

    added function for adding a product to DB
    add att url http://localhost:3000/admin/add-product, see result in the DB

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,12 +14,17 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);// when creating a product first time we pass null as a prodect id, in the function save() in the Product class the proper id will be added
-  product.save()
-  .then(()=> {
-    res.redirect('/');
-
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description
   })
-  .catch((err) => console.log(err))
+  .then(result => {
+    // console.log(`result`, result)
+    console.log(`Created product`)
+  })
+  .catch(err => console.log(err))
 };
 
 exports.getEditProduct = (req, res, next) => {
