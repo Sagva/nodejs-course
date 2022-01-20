d2bd3c76f7fb9132e8baafaf039090ee1df2172c
Elena <sagva2014@gmail.com>
2022-01-20 16:08:32 +0100

    Defining a model

--- models/product.js
+++ models/product.js
@@ -1,35 +1,27 @@
+const Sequelize = require('sequelize')
 
-const db = require('../util/database')//give access to the pool
-const Cart = require('./cart');
+const sequelize = require('./util/database')
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
+})
 
-  save() {
-    return db.execute(
-      // question marks used to secure inserting of data, Packsge mysql2 will secure replace them with data
-      'INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.description, this.imageUrl]
-    )
-    
-  }
-
-  static deleteById(id) {
-    
-  }
-
-  static fetchAll() {
-   return db.execute('SELECT * FROM products')
-  }
-
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id])
-  }
-};
+module.exports = Product
