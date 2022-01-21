a964858d5f98244f43be98c8a99cd3af6a986a17
Elena <sagva2014@gmail.com>
2022-01-21 10:29:47 +0100

    creating a dummy user

--- app.js
+++ app.js
@@ -20,6 +20,16 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//will run on incoming request, so the user will be already exist, because we create it on starting the app (not on incoming request)
+app.use((req,res, next) => {
+    User.findByPk(1)
+    .then(user => {//user here is a sequelize object with all available methods
+        req.user = user //storing th user we retrieved fron DB in the request
+        next()
+    })
+    .catch(err => console.log(err))
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -29,10 +39,20 @@ app.use(errorController.get404);
 Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'})
 User.hasMany(Product)
 
-sequelize.sync({force: true}) //sync method has a look at all the models you defined (we defined the model in './util/database')
-//and then it creates tables for them. It syncs our modals to the database by creating the appropriate tables and relations (if you have them)
+// sequelize.sync({force: true}) //{force: true} will overwrite tables every time the app starts
+sequelize.sync() //runs on the app start
 .then(result => {
     // console.log(`result`, result)
+    return User.findByPk(1) //check if we already have a user with id = 1 and if we have it we will not create a new one. If we don't have then we will create a new one
+})
+.then(user => {
+    if(!user) {// if this is null, create a new user
+        return User.create({name: 'Elena', email: 'test@test.com'}) //return a promise
+    }
+    return user //if user exist return it. Returns an js object
+})
+.then(user => {
+    console.log(`user`, user)
     app.listen(3000);
 })
 .catch(err => console.log(err)) 
