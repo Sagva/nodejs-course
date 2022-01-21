bd210f3d4f9989795b32513396e24cb825b68020
Elena <sagva2014@gmail.com>
2022-01-21 12:01:34 +0100

    Adding new product in the cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -56,11 +56,37 @@ exports.getCart = (req, res, next) => {
 };
 
 exports.postCart = (req, res, next) => {
-  const prodId = req.body.productId;
-  Product.findByPk(prodId, (product) => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect("/cart");
+  const prodId = req.body.productId
+  let fetchedCart
+  let newQuantity = 1
+  req.user
+    .getCart()
+    .then(cart => {
+      fetchedCart = cart
+      return cart.getProducts({ where: { id: prodId } })// got array with all products with same ID
+    })
+    .then(products => { 
+      let product
+      if (products.length > 0) { //if array several element, then we need to increase quantaty
+        product = products[0]
+      }
+
+      if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1
+        return product
+      }
+      return Product.findById(prodId)
+    })
+    .then(product => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity }
+      })
+    })
+    .then(() => {
+      res.redirect('/cart')
+    })
+    .catch(err => console.log(err))
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
