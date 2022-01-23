667910048cc3c923e7f1c4db33a346e596f5b381
Elena <sagva2014@gmail.com>
2022-01-23 16:11:10 +0100

    Displaying right nav items for logged in user

--- app.js
+++ app.js
@@ -44,14 +44,18 @@ app.use(
   })
 );
 
-// app.use((req, res, next) => {
-//   User.findById("61ed3e90ad401944b8dc530a")
-//     .then((user) => {
-//       req.user = user;
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      console.log(`user`, user);
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
