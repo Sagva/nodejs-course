34bbf309549e22a878faacc3a71212384e6ef49a
Elena <sagva2014@gmail.com>
2022-01-23 18:31:55 +0100

    using middleware to protect routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,24 +1,27 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const adminController = require('../controllers/admin');
+const adminController = require("../controllers/admin");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', adminController.getAddProduct);
+//you can add as many arguments(handlers) as you want. They will be parsed from left to right.
+//Inside isAuth the user might be redirected (if the user is not logged in), which means that request will never continue to the next controller action. In case if check will be passed inside middleare, we do call next.
+router.get("/add-product", isAuth, adminController.getAddProduct);
 
 // /admin/products => GET
-router.get('/products', adminController.getProducts);
+router.get("/products", isAuth, adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', adminController.postAddProduct);
+router.post("/add-product", isAuth, adminController.postAddProduct);
 
-router.get('/edit-product/:productId', adminController.getEditProduct);
+router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
 
-router.post('/edit-product', adminController.postEditProduct);
+router.post("/edit-product", isAuth, adminController.postEditProduct);
 
-router.post('/delete-product', adminController.postDeleteProduct);
+router.post("/delete-product", isAuth, adminController.postDeleteProduct);
 
 module.exports = router;
