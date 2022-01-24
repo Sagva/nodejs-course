7d00d4dfc59ad9c6ef2802e25970542f9932308c
Elena <sagva2014@gmail.com>
2022-01-24 09:58:42 +0100

    isAuthenticated added to every request to the lokals

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,15 +5,13 @@ exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: false,
   });
 };
 
 exports.getSignup = (req, res, next) => {
   res.render("auth/signup", {
     path: "/signup",
-    pageTitle: "Signup",
-    isAuthenticated: false,
+    pageTitle: "Signup"
   });
 };
 
