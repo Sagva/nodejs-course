bea78fefc592e39abc3ebbd269c00aecb52b4768
Elena <sagva2014@gmail.com>
2022-01-24 14:21:18 +0100

    added async validation/checking existence existanse of email while signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -75,7 +75,6 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
   const errors = validationResult(req)
   if(!errors.isEmpty()){
     console.log(`errors.array()`, errors.array())
@@ -85,13 +84,7 @@ exports.postSignup = (req, res, next) => {
       errorMessage: errors.array()[0].msg // msg: 'Invalid value' by default. Msg can be set eith method 'withMessage'
     })
   }
-  User.findOne({ email: email })
-    .then(userDoc => {
-      if (userDoc) {
-        req.flash('error', 'E-Mail exists already, please pick a different one.');
-        return res.redirect('/signup');
-      }
-      return bcrypt
+   bcrypt
         .hash(password, 12)
         .then(hashedPassword => {
           const user = new User({
@@ -112,7 +105,6 @@ exports.postSignup = (req, res, next) => {
           
         })
         .catch(err => console.log(err))
-    })
     .catch(err => {
       console.log(err);
     });
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const {check, body} = require('express-validator/check')
+const User = require('../models/user')
 
 const authController = require('../controllers/auth');
 
@@ -12,29 +13,33 @@ router.get('/signup', authController.getSignup);
 router.post('/login', authController.postLogin);
 
 router.post('/signup', 
-    [check('email') //this email we would extract that email from the cookies, the headers,body so anywhere 
+    [
+        check('email') //this email we would extract that email from the cookies, the headers,body so anywhere 
         .isEmail()
         .withMessage('Please enter a valid email ')
-        .custom((value, {req}) => {
-            if(value === 'test@test.com') {
-                throw new Error('This email address is forbidden')
+        .custom((value, { req }) => {
+            return User.findOne({ email: value }).then(userDoc => {
+              if (userDoc) {
+                return Promise.reject(
+                  'E-Mail exists already, please pick a different one.'
+                );
+              }
+            });
+          }),
+        body(
+        'password', //will look up for password field in the body
+        'Please enter a password with only numbers and text and at least 5 charachters') // error message, instead using withMessage method
+        .isLength({min: 5})
+        .isAlphanumeric(),// without special charachters
+
+        body('confirmPassword').custom((value, {req}) => {
+            if(value !== req.body.password) {
+                throw new Error('Passwords have to match!')
             }
             return true
-        }),
-    body(
-    'password', //will look up for password field in the body
-    'Please enter a password with only numbers and text and at least 5 charachters') // error message, instead using withMessage method
-    .isLength({min: 5})
-    .isAlphanumeric(),// without special charachters
-
-    body('confirmPassword').custom((value, {req}) => {
-        if(value !== req.body.password) {
-            throw new Error('Passwords have to match!')
-        }
-        return true
-    })
+        })
     ],
-        authController.postSignup);//email is a name if input field that we want to check
+    authController.postSignup);//email is a name if input field that we want to check
 //validaton result will be avalable in the auth-controller by exporting there 
 
 router.post('/logout', authController.postLogout);
