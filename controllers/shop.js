483dcdadaf2147d8274216a19208c03dd83398c2
Elena <sagva2014@gmail.com>
2022-01-25 13:01:47 +0100

    added pagination to getProducts function

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,19 +10,37 @@ const { match } = require('assert');
 const ITEMS_PER_PAGE = 2
 
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
-    .then((products) => {
-      console.log(products);
-      res.render("shop/product-list", {
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
+    .then(products => {
+      res.render('shop/product-list', {
         prods: products,
-        pageTitle: "All Products",
-        path: "/products",
+        pageTitle: 'Products',
+        path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
-    .catch((err) => {
-      console.log(err);
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
+
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
@@ -38,8 +56,8 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  const page = req.query.page //'page' - because in the link it calls ?page=1
-  let totalItems
+  const page = +req.query.page || 1 //'page' - because in the link it calls ?page=1
+  let totalItems;
 
   Product.find()
   .countDocuments()
@@ -54,7 +72,7 @@ exports.getIndex = (req, res, next) => {
         pageTitle: "Shop",
         path: "/",
         csrfToken: req.csrfToken(),
-        totalProducts: totalItems,
+        currentPage: page,
         hasNextPage: ITEMS_PER_PAGE * page < totalItems,
         hasPreviousPage: page > 1,
         nextPage: page + 1,
