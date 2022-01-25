850cd1dd6e43c57947f13da044263d7942d2b033
Elena <sagva2014@gmail.com>
2022-01-25 11:35:23 +0100

    fixing a typo

--- controllers/admin.js
+++ controllers/admin.js
@@ -178,8 +178,6 @@ exports.getProducts = (req, res, next) => {
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId).then(product => {
-    console.log(`product`, product)
-
     if(!product) {
       next(new Error('Product not found'))
     }
--- util/file.js
+++ util/file.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 const deleteFile = (filePath) => {
-    fs.unllink(filePath, (err) => {
+    fs.unlink(filePath, (err) => {
         if(err) {
             throw(err)
         }
