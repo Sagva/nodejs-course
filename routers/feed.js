28fb88d34447960f89c3ac227e28cd503bfb320e
Elena <sagva2014@gmail.com>
2022-01-25 14:23:10 +0100

    implementing route setup

---
+++ routers/feed.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const feedController = require('../controllers/feed')
+const router = express.Router()
+
+// GET /feed/posts - these kind of requests would get handled by this controller
+router.get('/posts', feedController.getPosts)
+
+module.exports = router
