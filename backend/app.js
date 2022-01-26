26f0e33eca6133d6c173e5253bb2d3cff4251006
Elena <sagva2014@gmail.com>
2022-01-26 08:39:57 +0100

    installing mongoose, connecting it with DB

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,29 @@
-const express = require('express')
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const feedRoutes = require("./routers/feed");
 
-const bodyParser = require('body-parser')
+const app = express();
 
-const feedRoutes = require('./routers/feed')
-
-const app = express()
-
-app.use(bodyParser.json())// application/json
+app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.use('/feed', feedRoutes)
+app.use("/feed", feedRoutes);
 
-app.listen(8080)
+mongoose
+  .connect(
+    "mongodb+srv://Elena:NODE123456node@cluster0.w6ofb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
