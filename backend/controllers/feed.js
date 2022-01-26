b5ddc09dd92f07bb8ea4cfae42008ae1aba17338
Elena <sagva2014@gmail.com>
2022-01-26 10:18:31 +0100

    fetching a single post

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -2,20 +2,19 @@ const { validationResult } = require("express-validator/check");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: "1",
-        title: "First post",
-        content: "THis is the first post",
-        imageUrl: "images/book.png",
-        creator: {
-          name: "Elena",
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  Post.find() //fetching actusl posts fron DB
+    .then((posts) => {
+      console.log(`posts`, posts);
+      res
+        .status(200)
+        .json({ message: "Fetched posts successfuly", posts: posts });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+        next(err);
+      }
+    });
 };
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
@@ -32,7 +31,7 @@ exports.createPost = (req, res, next) => {
     //create post in db
     title: title,
     content: content,
-    imageUrl: "umages/book.png",
+    imageUrl: "images/book.png",
     creator: {
       name: "Elena",
     },
@@ -49,9 +48,32 @@ exports.createPost = (req, res, next) => {
     })
     .catch((err) => {
       if (!err.statusCode) {
+        //check if we have an error status. If not - add it
         err.statusCode = 500; //server-side error
         // throw error throwing an error will not do the trick, it will not reach the next error handling middleware. Instead we have do use next(err)
         next(err); //will reach the next error handling express middleware. In app.js we need to register that middleware
+      }
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId; //this propery name (postId) has to match exactly to name we assign in the route "/post/:postId" (after the colon)
+
+  //find the post with extructed postId in the DB
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        //if the post is not found
+        const error = new Error("Could not find post");
+        error.statusCode = 404;
+        throw error; //if you throw an error inside of a "then" block, the next 'catch' block will be reached and that error will be passes as an error to the catch block
+      }
+      res.status(200).json({ message: "Post fetched", post: post });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+        next(err);
       }
     });
 };
