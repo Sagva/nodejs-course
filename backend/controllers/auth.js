1f582f9a3bdc7bbf84a8c3126fd874cb42e57995
Elena <sagva2014@gmail.com>
2022-01-26 16:00:04 +0100

    generate a token

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,7 @@
 const { validationResult } = require("express-validator/check");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const User = require("../models/user");
 
 exports.signup = (req, res, next) => {
@@ -60,6 +62,17 @@ exports.login = (req, res, next) => {
         error.statusCode = 401;
         throw error;
       }
+
+      // password is correct, generate token
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString(),
+        },
+        "somesupersecretsecret",
+        { expiresIn: "1h" } //token becomes invalid after 1h
+      );
+      res.status(200).json({ token: token, userId: loadedUser._id.toString() });
     })
     .catch((err) => {
       if (!err.statusCode) {
