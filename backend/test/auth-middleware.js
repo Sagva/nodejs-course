294310bf6878837141db4f05683fe358175f9631
Elena <sagva2014@gmail.com>
2022-01-27 14:21:15 +0100

    using stubs

--- backend/test/auth-middleware.js
+++ backend/test/auth-middleware.js
@@ -2,6 +2,7 @@
 const expect = require('chai').expect
 const authMiddleware = require('../middleware/is-auth')
 const jwt = require('jsonwebtoken')
+const sinon = require('sinon') // package that allows us to create a so-called step, which is a replacement for the original function where we can easily restore the original function.
 
 //describe function is for grouping the tests
 describe('Auth middleware', function() { //now in the termonal we can 
@@ -28,6 +29,25 @@ describe('Auth middleware', function() { //now in the termonal we can
         expect(authMiddleware.bind(this, {}, ()=> {})).to.throw() //bind does that we are not calling authMiddleware here, but reference to it 
         // expect(authMiddleware.bind(this, {}, ()=> {})).not.to.throw() //will not pass because it will be error and we are saying that we are not expecting an error
     })
+
+    it('should yield a userId after decoding the token', function() {
+        const req = {
+            get: function() {
+                return 'Bearer xsdfsdfssdfyz'
+            }
+        }
+
+        sinon.stub(jwt, 'verify') //two arguments: 1) JWT-the object that has the method, 2) verify is the actual method
+
+        jwt.verify.returns({userId: 'abc'}) //returns is method added by simon, it allows us to configure what that function should return
+        authMiddleware(req, {}, ()=> {})
+        expect(req).to.have.property('userId') //we expect that property because we are addng it in the authMiddleware  
+        expect(req).to.have.property('userId', 'abc') 
+        expect(jwt.verify.called).to.be.true //check if the method was called in our authMiddleware
+
+        jwt.verify.restore() // now the method will be restored to it original behavior
+    })
+
     it('should throw an error if the token cannot be verified', function() {
         const req = {
             get: function() {
@@ -37,19 +57,7 @@ describe('Auth middleware', function() { //now in the termonal we can
 
         expect(authMiddleware.bind(this, {}, ()=> {})).to.throw() 
     })
-    it('should yield a userId after decoding the token', function() {
-        const req = {
-            get: function() {
-                return 'Bearer xsdfsdfssdfyz'
-            }
-        }
-        jwt.verify = function() {//we're globaly overwriting the actual verify method that this package has => in all other authMiddleware tests that come after that line  it will work wrong 
-            return {userId: 'abc'} //we will get userId in the authMiddleware => test will pass
-            //instead of manually stabbing or mocking functionalities and replacing them it's better to use packages that allows you to restore gte original setup
-        }
-        authMiddleware(req, {}, ()=> {})
-        expect(req).to.have.property('userId') //we expect that property because we are addng it in the authMiddleware  
-    })
+    
 
 
 
