71b32d3551aba41d4fbb9522e57a36fe736df397
Elena <sagva2014@gmail.com>
2022-01-27 13:29:46 +0100

    testing the Auth middleware

---
+++ test/auth-middleware.js
@@ -0,0 +1,18 @@
+
+const expect = require('chai').expect
+const authMiddleware = require('../middleware/is-auth')
+
+// unit test, testing only one function
+it('should throw an error if no authorization header is present', function() {
+    //we create a dummy request
+    const req = {
+        get: function() {//we want to test scenario when get authorization does not return an authorization header 
+            return null//it does not return a value for our authorization call 
+        }
+    }
+
+    authMiddleware(bind(this, req, {}, ()=> {})) //second arg - response object , 3d: next func which we don't execute
+    .to.throw('Not authenticated')
+})
+
+//integration test tests more complete flow
