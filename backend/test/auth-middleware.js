8a51b3dea68bad91af14eb25ad4cc9dfee550d00
Elena <sagva2014@gmail.com>
2022-01-27 13:46:41 +0100

    organazing multiple tests

--- backend/test/auth-middleware.js
+++ backend/test/auth-middleware.js
@@ -2,7 +2,9 @@
 const expect = require('chai').expect
 const authMiddleware = require('../middleware/is-auth')
 
-// unit test, testing only one function
+//describe function is for grouping the tests
+describe('Auth middleware', function() { //now in the termonal we can 
+
 it('should throw an error if no authorization header is present', function() {
     //we create a dummy request
     const req = {
@@ -24,4 +26,5 @@ it('should throw an error if the authorizantion header is only obe string', func
 
     expect(authMiddleware.bind(this, {}, ()=> {})).to.throw() //bind does that we are not calling authMiddleware here, but reference to it 
     // expect(authMiddleware.bind(this, {}, ()=> {})).not.to.throw() //will not pass because it will be error and we are saying that we are not expecting an error
+})
 })
