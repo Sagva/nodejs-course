73122edacbc390e5fea540d9d16c1322b95c6948
Elena <sagva2014@gmail.com>
2022-01-28 09:56:17 +0100

    configuring TypeScript

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
+"use strict";
 //npm install -g typescript //command will only work if node js is installed on the pc
 //tsc app.ts - command for starting the script => will complile the code to js and insert the app.js file in the root folder
-var num1Elem = document.getElementById('num1');
-var num2Elem = document.getElementById('num2');
-var buttonElem = document.querySelector('button');
+const num1Elem = document.getElementById('num1');
+const num2Elem = document.getElementById('num2');
+const buttonElem = document.querySelector('button');
 function add(num1, num2) {
     return num1 + num2;
 }
-buttonElem.addEventListener('click', function () {
-    var num1 = num1Elem.value; //value always returns a string
-    var num2 = num2Elem.value;
-    var result = add(+num1, +num2);
-    console.log(result);
-});
+if (buttonElem) {
+    buttonElem.addEventListener('click', () => {
+        const num1 = num1Elem.value; //value always returns a string
+        const num2 = num2Elem.value;
+        const result = add(+num1, +num2);
+        console.log(result);
+    });
+}
 console.log(add(1, 6));
 // console.log(add('1', '6')); error , args are strings, func expects numbers
--- app.ts
+++ app.ts
@@ -1,20 +1,23 @@
 //npm install -g typescript //command will only work if node js is installed on the pc
-//tsc app.ts - command for starting the script => will complile the code to js and insert the app.js file in the root folder
+//tsc command for compiling all files in the folder with config file in consideration
 const num1Elem = document.getElementById('num1') as HTMLInputElement
 const num2Elem = document.getElementById('num2') as HTMLInputElement
-const buttonElem = document.querySelector('button')
+const buttonElem = document.querySelector('button')! //! = we know that it's not going to be null
 
 function add(num1: number, num2: number) {
   return num1 + num2;
 }
 
-buttonElem.addEventListener('click', ()=> {
-  const num1 = num1Elem.value //value always returns a string
-  const num2 = num2Elem.value
+if(buttonElem) {
 
-  const result = add(+num1, +num2)
-  console.log(result)
-})
+  buttonElem.addEventListener('click', ()=> {//Object is possibly 'null' - strict mode in action
+    const num1 = num1Elem.value //value always returns a string
+    const num2 = num2Elem.value
+  
+    const result = add(+num1, +num2)
+    console.log(result)
+  })
+}
 
 console.log(add(1, 6));
 
