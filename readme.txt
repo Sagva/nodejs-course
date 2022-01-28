d75f01f17aa2f52cf74539dc48a24f0a3173b3db
Elena <sagva2014@gmail.com>
2022-01-28 13:49:49 +0100

    moving to a better project structure

--- readme.txt
+++ readme.txt
@@ -17,3 +17,9 @@ npm install --save-dev @types/express
 comment out "moduleResolution": "node",  in the tsconfig.json
 
 npm install --save-dev body-parser
+
+comment out "outDir": "./dist" in the tsconfig file (add /dist) //is the folder where the compiled code should end up 
+delete previous compiled js files
+move routes, models and app.ts into src folder 
+comment out  "rootDir": "./src" in the tsconfig file (add ./src) // the folder that contains our TypeScript code.
+, run tsc => now js files appears only inside dist folder
---
+++ src/routes/todos.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+let todos = []; //array full of Todos
+const router = (0, express_1.Router)();
+router.get('/', (req, res, next) => {
+    res.status(200).json({ todos: todos });
+});
+router.post('/todo', (req, res, next) => {
+    const body = req.body;
+    const newTodo = {
+        id: new Date().toISOString(),
+        text: body.text
+    };
+    todos.push(newTodo);
+    res.status(200).json({ message: "Added todo", todo: newTodo, todos: todos });
+});
+router.put('/todo/:todoId', (req, res, next) => {
+    const params = req.params;
+    const todoId = params.todoId;
+    const body = req.body;
+    const todoIndex = todos.findIndex(todoItem => todoItem.id === todoId);
+    if (todoIndex >= 0) {
+        todos[todoIndex] = { id: todos[todoIndex].id, text: body.text };
+        return res.status(200).json({ message: "Updated todo", todos: todos });
+    }
+    res.status(404).json({ message: "Could not find todo for this Id" });
+});
+router.delete('/todo/:todoId', (req, res, next) => {
+    todos = todos.filter(todoItem => todoItem.id !== req.params.todoId);
+    res.status(200).json({ message: "Deleted todo", todos: todos });
+});
+exports.default = router; //instead module.exports = router
