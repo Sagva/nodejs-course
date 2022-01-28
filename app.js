02c6ea6e7c3818366d02c344964386162632859c
Elena <sagva2014@gmail.com>
2022-01-28 11:11:13 +0100

    setting up Node and TypeScript

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+"use strict";
 // const express = require('express') //require method is only availablewhen we're running this code with Node.
 //If we would want to run it in the browser, this does not exist. TypeScript does not know where we plan to run this code,
 //to let TypeScript know that require method exists,we can run this command npm install --save-dev @types/node
-
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 // import express = require('express') or
-import express from 'express' //will compiled to require('express') 
-
-const app = express()
-
-app.listen({port: 3000})
+const express_1 = __importDefault(require("express")); //will compiled to require('express') 
+const app = (0, express_1.default)();
+app.listen({ port: 3000 });
--- readme.txt
+++ readme.txt
@@ -4,3 +4,14 @@ tsc --init
 npm init
 npm install --save express
 npm install --save body-parser
+
+used packeges:
+npm install --save-dev @types/node //to let TypeScript know that require method exists,we can run this command
+
+On npmjs.com, you will find many @types packages.These are packages which provide TypeScript translations
+for JavaScript features. They contain code that allows TypeScript to understand a certain library,
+a certain package or a certain commands
+
+npm install --save-dev @types/express
+
+comment out "moduleResolution": "node",  in the tsconfig.json
