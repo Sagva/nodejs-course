d7d3769fb2d72823c9593a9c59ed517beb5efd1d
Elena <sagva2014@gmail.com>
2022-01-28 13:18:58 +0100

    fixng a bug in the routes

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 "use strict";
-// const express = require('express') //require method is only availablewhen we're running this code with Node.
-//If we would want to run it in the browser, this does not exist. TypeScript does not know where we plan to run this code,
-//to let TypeScript know that require method exists,we can run this command npm install --save-dev @types/node
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// import express = require('express') or
-const express_1 = __importDefault(require("express")); //will compiled to require('express') 
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const todos_1 = __importDefault(require("./routes/todos"));
 const app = (0, express_1.default)();
+app.use(body_parser_1.default.json());
+app.use(todos_1.default);
 app.listen({ port: 3000 });
--- routes/todos.ts
+++ routes/todos.ts
@@ -10,14 +10,14 @@ router.get('/', (req, res, next) => {
 })
 router.post('/todo', (req, res, next) => {
     const newTodo: Todo = {
-        id: new Date().toLocaleDateString(),
+        id: new Date().toISOString(),
         text: req.body.text
     }
     todos.push(newTodo)
     res.status(200).json({message: "Added todo", todo: newTodo, todos: todos})
 })
 
-router.put('todo/:todoId', (req, res, next) => {
+router.put('/todo/:todoId', (req, res, next) => {
     const todoId = req.params.todoId
     const todoIndex = todos.findIndex(todoItem => todoItem.id === todoId)
     if(todoIndex >= 0) {
@@ -26,7 +26,7 @@ router.put('todo/:todoId', (req, res, next) => {
     }
     res.status(404).json({message: "Could not find todo for this Id"})
 })
-router.delete('todo/:todoId', (req, res, next) => {
+router.delete('/todo/:todoId', (req, res, next) => {
     todos = todos.filter(todoItem => todoItem.id !== req.params.todoId)
     res.status(200).json({message: "Deleted todo", todos: todos})
 })
